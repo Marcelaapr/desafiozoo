75813cbf23cbfc6beaa96d641a915f7ff10fccd0
Marcela Rodriguez <marcelaaprodriguez@gmail.com>
2022-09-17 16:16:42 -0300

    criando desafio do zoo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Zoo World</h1>
+      <ul>
+        <a href = ""><li>Conheça nossos Animais</li></a>
+        <li>Informações Gerais</li>
+      </ul>
+      <img src = "https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?auto=compress&cs=tinysrgb&w=600" alt = "Zoo World" />
+    <p>
+    É um ótimo lugar para dar uma pausa na praia e criar memórias familiares duradouras! ZooWorld Zoological Park, localizado na bela Praia da Cidade do Panamá, Flórida é o lar de animais exóticos de todo o mundo e você não precisará de binóculos para vê-los! Acaricie uma preguiça, segure um canguru, beije uma girafa, aconchegue um lêmure... ou mesmo acariciar um porco-espinho!
+    </p>
+
     </div>
   );
 }
