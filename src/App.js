c781f7fdf534b46129b31f41558fc96df549007c
Marcela Rodriguez <marcelaaprodriguez@gmail.com>
2022-09-17 17:47:43 -0300

    atualizando estilização

--- src/App.js
+++ src/App.js
@@ -6,13 +6,21 @@ function App() {
       <h1>Zoo World</h1>
       <ul>
         <a href = ""><li>Conheça nossos Animais</li></a>
-        <li>Informações Gerais</li>
+        <a href = ""><li>Galeria de Fotos</li></a>
       </ul>
-      <img src = "https://images.pexels.com/photos/247502/pexels-photo-247502.jpeg?auto=compress&cs=tinysrgb&w=600" alt = "Zoo World" />
+      <br/>
+      <img src = "https://images.pexels.com/photos/6949503/pexels-photo-6949503.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt = "girafa"/>
+      <br/>
+      <br/>
     <p>
-    É um ótimo lugar para dar uma pausa na praia e criar memórias familiares duradouras! ZooWorld Zoological Park, localizado na bela Praia da Cidade do Panamá, Flórida é o lar de animais exóticos de todo o mundo e você não precisará de binóculos para vê-los! Acaricie uma preguiça, segure um canguru, beije uma girafa, aconchegue um lêmure... ou mesmo acariciar um porco-espinho!
+    É um ótimo lugar para dar uma pausa na praia e criar memórias familiares duradouras! <b>ZooWorld Zoological Park</b>, localizado na bela Praia da Cidade do Panamá, Flórida é o lar de animais exóticos de todo o mundo e você não precisará de binóculos para vê-los! Acaricie uma preguiça, segure um canguru, beije uma girafa, aconchegue um lêmure... ou mesmo acariciar um porco-espinho!
     </p>
-
+    <br/>
+    <img src = "https://images.pexels.com/photos/4835274/pexels-photo-4835274.jpeg?auto=compress&cs=tinysrgb&w=600" alt = "preguica"></img>
+    <img src = "https://images.pexels.com/photos/5840732/pexels-photo-5840732.jpeg?auto=compress&cs=tinysrgb&w=600" alt="canguru"/>
+    <img src = "https://images.pexels.com/photos/7336630/pexels-photo-7336630.jpeg?auto=compress&cs=tinysrgb&w=600" alt = "lemure"></img>
+  
+    <br/>
     </div>
   );
 }
